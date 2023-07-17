1ba11b205a18632da5bd27035fd0d37b592c6a9b
wlsdks12 <wlsdks12@naver.com>
2023-07-17 10:24:53 +0900

    1. import 최적화

--- board-project-admin/src/test/java/com/fastcampus/boardprojectadmin/controller/MainControllerTest.java
+++ board-project-admin/src/test/java/com/fastcampus/boardprojectadmin/controller/MainControllerTest.java
@@ -6,10 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
