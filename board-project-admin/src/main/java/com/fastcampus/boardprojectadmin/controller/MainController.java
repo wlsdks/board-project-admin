0d5a098a0dccddfbcf7b6a18682775f042c92d82
wlsdks12 <wlsdks12@naver.com>
2023-07-17 10:50:57 +0900

    1. 컨트롤러 기본코드 추가

--- board-project-admin/src/main/java/com/fastcampus/boardprojectadmin/controller/MainController.java
+++ board-project-admin/src/main/java/com/fastcampus/boardprojectadmin/controller/MainController.java
@@ -1,11 +1,15 @@
 package com.fastcampus.boardprojectadmin.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /** root 페이지 컨트롤러 */
-@RequestMapping("/admin/members")
 @Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        return "forward:/management/articles";
+    }
 
 }
